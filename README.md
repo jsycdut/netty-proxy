dc4e3e2ca35540f0508559914f8ff3bb94f1637a
jsycdut <jsycdut@gmail.com>
2026-06-08 18:19:13 +0800

    feat: basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # netty-proxy
 netty based proxy tool
+## goal
+* [ ] local socks5 proxy tool
+* [ ] c/s architecture
+* [ ] encryption
---
+++ proxy-client/src/main/java/org/netty/io/proxy/client/App.java
@@ -0,0 +1,10 @@
+package org.netty.io.proxy.client;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
